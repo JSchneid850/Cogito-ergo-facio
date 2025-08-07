99b1046054cb48827421c1c9f61a363549e942a5
JSchneid850 <joshua.d.schneider@gmail.com>
2025-08-07 11:35:43 -0500

    102 times the charm

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1754583332',resources: [
+    cacheName: 'chirpy-1754584525',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
