efbd4228fc7b960f2aad5f070f1ab396688ce7e1
JSchneid850 <joshua.d.schneider@gmail.com>
2025-08-07 11:59:22 -0500

    A few updates

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1754585335',resources: [
+    cacheName: 'chirpy-1754585912',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
