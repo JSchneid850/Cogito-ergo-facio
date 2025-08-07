9eba93a6074d103032e2657c824dda168e9e2cec
JSchneid850 <joshua.d.schneider@gmail.com>
2025-08-07 10:57:29 -0500

    update .gitignore

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1754581974',resources: [
+    cacheName: 'chirpy-1754582221',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
