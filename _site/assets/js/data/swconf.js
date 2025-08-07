c0c91ac331a3ac58f26712799cbcd793d65f30b6
JSchneid850 <joshua.d.schneider@gmail.com>
2025-08-07 11:41:33 -0500

    Forget trying to 'mock' a cdn wth a gh link just drop the whole url in there

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1754584525',resources: [
+    cacheName: 'chirpy-1754584833',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
