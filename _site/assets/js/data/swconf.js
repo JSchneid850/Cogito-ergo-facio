36bfa81f85313b09a601664887dc487cb43637c0
JSchneid850 <joshua.d.schneider@gmail.com>
2025-08-07 11:49:11 -0500

    will this pick up the css? who knows

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1754584833',resources: [
+    cacheName: 'chirpy-1754585335',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
